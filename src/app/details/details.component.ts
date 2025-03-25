f9c8ae330645cf47c3ea849b32565c89e0c6590c
dumbshadok <dumbshadok@proton.me>
2025-03-25 22:52:32 +0100

    implemented details page and router logic

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id'])
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId)
+  }
 
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   template: `
   <section class="listing">
     <img
@@ -16,6 +17,7 @@ import { HousingLocation } from '../housing-location';
     />
     <h2 class="listing-heading">{{ housingLocationInput.name }}</h2>
     <p class="listing-location">{{ housingLocationInput.city }}, {{ housingLocationInput.state }}</p>
+    <a [routerLink]="['/details', housingLocationInput.id]">Learn More</a>
   </section>
   `,
   styleUrls: ['./housing-location.component.css'],
